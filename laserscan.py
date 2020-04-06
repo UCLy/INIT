cafcc82d996832cf463c1211e53c1a9b39495451
Gabriel Fourtoy <47565196+gabfour@users.noreply.github.com>
2020-04-06 14:07:45 +0200

    Revert "Changed a bit laserscan and made a modification of fixedLaserscan that might or might not work"
    This reverts commit a65ebf666691d4be014b46cdb86b20a3afedfe99.

--- laserscan.py
+++ laserscan.py
@@ -5,24 +5,42 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
 def callback(msg):
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
-    vel_msg = Twist()
-	vel_msg.linear.x = 5
+        pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+        vel_msg = Twist()
+        vel_msg.linear.x = 0
 	vel_msg.linear.y = 0
 	vel_msg.linear.z = 0
 	vel_msg.angular.x = 0
 	vel_msg.angular.y = 0
 	vel_msg.angular.z = 0
-    if 0.01 < msg.ranges[0] < msg.range_max:
-        print("I see a hand at", msg.ranges[0], "m !")
-		vel_msg.linear.x = 0
-	    pub.publish(vel_msg)
-		sub.unregister()
+        if 0.01 < msg.ranges[0] < msg.range_max:
+                print("I see a hand at", msg.ranges[0], "m !")
+		vel_msg.linear.x = msg.ranges[0]-0.2
 	else:
 		print(msg.range_min, "<", msg.ranges[0],"<",msg.range_max)
-    pub.publish(vel_msg)
+
+        pub.publish(vel_msg)
+	        
+"""
+	print('===============================================')
+	print('s1 [0]')
+	print msg.ranges[0]
+
+	print('s2 [90]')
+	print msg.ranges[90]
+
+	print('s3 [180]')
+	print msg.ranges[180]
+
+	print('s4 [270]')
+	print msg.ranges[270]
+
+	print ('s5 [359]')
+	print msg.ranges[359]
+"""
 
 rospy.init_node('laser_data')
 sub = rospy.Subscriber('scan', LaserScan, callback)
 
 rospy.spin()
+

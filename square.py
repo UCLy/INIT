827225ee8986e5464a835f2bf9c77111148f2aa8
guthmoelle <56672394+guthmoelle@users.noreply.github.com>
2020-04-20 15:46:58 +0200

    Update square.py

--- square.py
+++ square.py
@@ -88,9 +88,6 @@ def move():
 
     #Receiveing the user's input
     print("Let's move your robot")
-    #speed = input("Input your speed:")
-    #distance = input("Type your distance:")
-    #isForward = input("Foward?: ")#True or False
 
     speed = 0.5
     distance = 0.01
@@ -107,25 +104,7 @@ def move():
     vel_msg.angular.x = 0
     vel_msg.angular.y = 0
     vel_msg.angular.z = 0
-
-    #while not rospy.is_shutdown():
-
-    #Setting the current time for distance calculus
-    #t0 = rospy.Time.now().to_sec()
-    #current_distance = 0
     
-    #Loop to move the turtle in an specified distance
-    #while(current_distance < distance):
-        #Publish the velocity
-        #velocity_publisher.publish(vel_msg)
-        #Takes actual time to velocity calculus
-        #t1=rospy.Time.now().to_sec()
-        #Calculates distancePoseStamped
-        #current_distance= speed*(t1-t0)
-        
-    #After the loop, stops the robot
-    #vel_msg.linear.x = 0
-    #Force the robot to stop
 
     for i in range(4):
         segment(velocity_publisher)
